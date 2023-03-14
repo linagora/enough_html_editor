34a72dc8c4cb5ed28b2427177593d3d6a43e5be2
dab246 <tdvu@linagora.com>
2023-03-15 00:48:20 +0700

    Set the exact location for the signature `(Below the input and above quoted message)`

--- lib/src/utils/javascript_utils.dart
+++ lib/src/utils/javascript_utils.dart
@@ -1,30 +1,34 @@
 
 const String jsHandleSignature = '''
   function insertSignature(signature) {
-    var nodeSignature = document.getElementsByClassName('tmail-signature');
+    const nodeSignature = document.getElementsByClassName('tmail-signature');
     if (nodeSignature.length <= 0) {
-      var nodeEditor = document.getElementById('editor');
-      var tagTop = document.createElement('br');
-      tagTop.setAttribute('class', 'tmail-break-tag');
-      nodeEditor.appendChild(tagTop);
-      var divSignature = document.createElement('div');
+      const nodeEditor = document.getElementById('editor');
+      
+      const divSignature = document.createElement('div');
       divSignature.setAttribute('class', 'tmail-signature');
       divSignature.innerHTML = signature;
-      nodeEditor.appendChild(divSignature);
-      var tagBottom = document.createElement('br');
-      tagBottom.setAttribute('class', 'tmail-break-tag');
-      nodeEditor.appendChild(tagBottom);
+      
+      const listHeaderQuotedMessage = nodeEditor.querySelectorAll('cite');
+      const listQuotedMessage = nodeEditor.querySelectorAll('blockquote');
+      
+      if (listHeaderQuotedMessage.length > 0) {
+        nodeEditor.insertBefore(divSignature, listHeaderQuotedMessage[0]);
+      } else if (listQuotedMessage.length > 0) {
+        nodeEditor.insertBefore(divSignature, listQuotedMessage[0]);
+      } else {
+        nodeEditor.appendChild(divSignature);
+      }
     } else {
       nodeSignature[0].innerHTML = signature;
     }
   }
 
   function removeSignature() {
-    var nodeSignature = document.getElementsByClassName('tmail-signature');
+    const nodeSignature = document.getElementsByClassName('tmail-signature');
     if (nodeSignature.length > 0) {
       nodeSignature[0].remove();
     }
-    document.querySelectorAll(".tmail-break-tag").forEach(el => el.remove());
   }
   
 ''';
